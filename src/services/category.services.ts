4402a0ef71c4850f36900e49f6215130e7d033eb
Jonas <jonasafribeiro@gmail.com>
2023-05-12 14:01:40 -0300

    feat: change type of variable to not use any type

--- src/services/category.services.ts
+++ src/services/category.services.ts
@@ -2,14 +2,15 @@ import { categoryRepo } from "../data-source";
 
 const categoryServices = {
   getAllCategoryRelations: async (categoryId: number) => {
-    const category = (await categoryRepo
+    const category = await categoryRepo
       .createQueryBuilder("category")
-      .leftJoinAndSelect("category.realEstates", "realEstate")
+      .leftJoinAndMapMany(
+        "category.realEstate",
+        "category.realEstates",
+        "realEstate"
+      )
       .where("category.id = :id", { id: categoryId })
-      .getOne()) as any;
-
-    category["realEstate"] = category["realEstates"];
-    delete category["realEstates"];
+      .getOne();
 
     return category;
   },
